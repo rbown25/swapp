87ccd912d877267e42794754548066f26f5b047e
rbown@cantab.net <rbown@cantab.net>
2025-06-06 07:55:44 +0000

    Created Form1.ItemTemplate2; Edited settings and Form1; Deleted Form1.ItemTemplate1 and Form1.RowTemplate1

--- client_code/Form1/ItemTemplate2/__init__.py
+++ client_code/Form1/ItemTemplate2/__init__.py
@@ -1,17 +1,13 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import ItemTemplate2Template
 from anvil import *
+import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-import anvil.users
-import anvil.server
 
-class Form1(Form1Template):
+class ItemTemplate2(ItemTemplate2Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-    anvil.users.login_with_form()
-
-    self.PC_list.items = app_tables.pcs.search()
