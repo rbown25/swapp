5322fb01c86509cdd2e98d6cc3f613b947ef934e
rbown@cantab.net <rbown@cantab.net>
2025-06-30 11:40:11 +0000

    Edited settings and ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -1,8 +1,11 @@
+import anvil.files
+from anvil.files import data_files
 import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
+import csv
 
 # This is a server module. It runs on the Anvil server,
 # rather than in the user's browser.
@@ -18,7 +21,20 @@ import anvil.server
 #
 @anvil.server.callable
 def import_new_users():
-  '''This function will import any new users added to the "Email_import" table to the Users table.
+  '''This function will import any new users in the csv file "innovians.csv" to the Users table.
   It is intended to be run via the server REPL. To do this type:
   anvil.server.call('import_new_users')'''
-  print("Hello world")
+  all_staff=[]
+  current_staff=[]
+  file = open(data_files['innovians.csv'], "rU")
+  reader = csv.reader(file, delimiter=',')
+  for row in reader:
+    all_staff.append(row[0])
+  #text = file.read()
+  for row in app_tables.users.search():
+    current_staff.append(row["email"])
+  staff_to_add=list(set(all_staff).difference(current_staff))
+
+  for person in staff_to_add:
+    row = app_tables.users.add_row(email=person,enabled=True)
+    print(row['email'])
